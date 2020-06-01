c02555465d3ad7e1ea92081ca3e9c432f1cb50a4
Sam Ireland <sam.ireland.uk@gmail.com>
2020-06-01 16:45:30 +0100

    Add forms for new properties

--- core/forms.py
+++ core/forms.py
@@ -49,4 +49,62 @@ class LoginForm(Form):
             if user: return user
         self.add_error("email", "Invalid credentials.")
         return False
+
+
+
+class UpdateEmailForm(ModelForm):
+    """Updates a user's email."""
+
+    class Meta:
+        model = User
+        fields = ["email"]
+    
+    password = CharField(required=True)
+
+    def clean_password(self):
+        """Checks that the supplied password is currect."""
+
+        if not self.instance.check_password(self.data["password"]):
+            self.add_error("password", "Password not correct.")
+        return self.data["password"]
+
+
+
+class UpdatePasswordForm(ModelForm):
+
+    class Meta:
+        model = User
+        fields = []
+    
+    current = CharField(required=True)
+    new = CharField(required=True)
+
+    def clean_current(self):
+        """Checks that the supplied current password is currect."""
+
+        if not self.instance.check_password(self.data["current"]):
+            self.add_error("current", "Current password not correct.")
+        return self.data["current"]
+
+
+    def clean_new(self):
+        """Runs the password validators specified in settings."""
+
+        validate_password(self.data["new"])
+        return self.data["new"]
+
+
+    def save(self):
+        self.instance.set_password(self.cleaned_data.get("new"))
+        self.instance.save()
+
+
+
+class UpdateBasicSettingsForm(ModelForm):
+    """Updates a user's basic settings."""
+
+    class Meta:
+        model = User
+        fields = ["timezone", "day_ends"]
+
         
--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -57,3 +57,115 @@ class LoginFormTests(TestCase):
         john.save()
         form = LoginForm({"email": "john@gmail.com", "password": "12345678"})
         self.assertEqual(form.validate_credentials(), john)
+
+
+
+class LoginFormTests(TestCase):
+
+    def test_login_form_can_reject(self):
+        form = LoginForm({"email": "john@john.com", "password": "123456789"})
+        self.assertFalse(form.validate_credentials())
+        self.assertIn("Invalid credentials", form.errors["email"][0])
+
+
+    def test_login_form_can_login(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("12345678")
+        john.save()
+        form = LoginForm({"email": "john@gmail.com", "password": "12345678"})
+        self.assertEqual(form.validate_credentials(), john)
+
+
+
+class UpdateEmailFormTests(TestCase):
+
+    def test_form_can_update_email(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        form = UpdateEmailForm({"email": "new@email.com", "password": "password"}, instance=john)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(john.email, "new@email.com")
+    
+
+    def test_form_can_reject_email(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        mixer.blend(User, email="sam@gmail.com")
+        form = UpdateEmailForm({"email": "sam@gmail.com", "password": "password"}, instance=john)
+        self.assertIn("already exists", form.errors["email"][0])
+        self.assertFalse(form.is_valid())
+        form = UpdateEmailForm({"email": "sam@gmail", "password": "password"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("valid email", form.errors["email"][0])
+    
+
+    def test_form_can_reject_password(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        form = UpdateEmailForm({"email": "new@email.com", "password": "password123"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Password not correct", form.errors["password"][0])
+
+
+
+class UpdatePasswordFormTests(TestCase):
+
+    def test_form_can_update_password(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        form = UpdatePasswordForm({"current": "password", "new": "warwick96"}, instance=john)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertTrue(john.check_password("warwick96"))
+    
+
+    def test_form_can_reject_current_password(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        form = UpdatePasswordForm({"current": "xxxxxxxxx", "new": "warwick96"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("password not correct", form.errors["current"][0])
+    
+
+    def test_form_can_reject_new_password(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        john.set_password("password")
+        form = UpdatePasswordForm({"current": "password", "new": "arwick96"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("9 characters", form.errors["new"][0])
+        form = UpdatePasswordForm({"current": "password", "new": "3738426578326"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("numeric", form.errors["new"][0])
+        form = UpdatePasswordForm({"current": "password", "new": "password1"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("too common", form.errors["new"][0])
+
+
+
+class UpdateBasicSettingsFormTests(TestCase):
+
+    def test_form_can_update_basic_settings(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        form = UpdateBasicSettingsForm({"day_ends": 4, "timezone": ""}, instance=john)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(john.day_ends, 4)
+        self.assertEqual(john.timezone, "")
+    
+
+    def test_form_can_reject_day_ends(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        form = UpdateBasicSettingsForm({"day_ends": -1, "timezone": ""}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("between 0 and 5", form.errors["day_ends"][0])
+        form = UpdateBasicSettingsForm({"day_ends": 6, "timezone": ""}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("between 0 and 5", form.errors["day_ends"][0])
+    
+
+    def test_form_can_reject_timezone(self):
+        john = mixer.blend(User, email="john@gmail.com")
+        form = UpdateBasicSettingsForm({"day_ends": 5, "timezone": "Europe/Bacup"}, instance=john)
+        self.assertFalse(form.is_valid())
+        self.assertIn("valid choice", form.errors["timezone"][0])
