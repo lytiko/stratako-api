a9549acd4d68e87652ec858aa488fec6ecfb7807
Sam Ireland <sam.ireland.uk@gmail.com>
2020-11-28 18:53:15 +0000

    Operation ordering

--- core/models.py
+++ core/models.py
@@ -4,11 +4,12 @@ class Operation(models.Model):
 
     class Meta:
         db_table = "operations"
-        ordering = ["-started"]
+        ordering = ["-started", "slot_order"]
 
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     slot = models.IntegerField(null=True)
+    slot_order = models.IntegerField(null=True)
     started = models.DateField(null=True)
     completed = models.DateField(null=True)
 
